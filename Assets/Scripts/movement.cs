8fe85b44b0aeaf0fb72416276cb7e7441f1fc24b
Shankar Anbalagan <bravoshankar@gmail.com>
2017-12-03 10:55:46 +0530

    Inital commit

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    Rigidbody2D rbd;
+    float horizontal, vertical;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rbd = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontal,vertical);
+
+        rbd.AddForce(movement*speed);
+
+        horizontal = vertical = 0F;
+
+    }
+}
